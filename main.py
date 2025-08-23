7d38c80b6973292a6687c1f23236fc8edaed3a53
samanjalili009-design <samanjalili009@gmail.com>
2025-08-23 04:07:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from telegram.ext import (
 # ===== تنظیمات =====
 BOT_TOKEN = "8145993181:AAFK7PeFs_9VsHqaP3iKagj9lWTNJXKpgjk"
 ACCOUNT_HASH = "f9e86b274826212a2712b18754fabc47"
-ALLOWED_USER_ID = 1867911
+ALLOWED_USER_ID = 417536686
 MAX_FILE_SIZE = 2097152000  # 2GB پیش‌فرض
 
 # Userbot (برای آپلود به حساب خودتان)
